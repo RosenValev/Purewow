724d548f60e9f70989733f540a6d0f461c3cc73e
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-11-06 13:53:45 +0200

    add login, logout to userService

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User.js');
 const generateToken = require('../utils/generateToken.js');
+const bcrypt = require('bcrypt');
 
 //GET ALL
 const getAll = async (req, res) => {
@@ -32,7 +33,7 @@ const getOne = async (req, res) => {
 };
 
 //CREATE
-const register = async (req, res) => {
+const createOne = async (req, res) => {
     try {
         const userData = req.body;
         const user = await User.findOne({ username: userData.username });
@@ -75,7 +76,6 @@ const updateOne = async (req, res) => {
 };
 
 //DELETE
-
 const deleteOne = async (req, res) => {
     try {
         const { id } = req.params;
@@ -88,16 +88,56 @@ const deleteOne = async (req, res) => {
         console.log(err.message);
         res.status(500).json({ message: err.message });
     }
-}
+};
+
+//LOGIN
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: `User ${username} not found}` });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(404).json({ message: "Username or password don't match!" });
+        }
+
+        const token = generateToken(user);
+        res.cookie('auth', token);
+        const data = {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            avatar: user.avatar,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+            token,
+        }
+        res.status(200).json(data);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+//LOGOUT
+const logout = (req, res) => {
+    res.clearCookie('auth');
+    res.status(200).json({ success: 'Logged out successfully!' });
+};
 
 
 
 
 
 module.exports = {
-    register,
+    createOne,
     getAll,
     getOne,
     updateOne,
     deleteOne,
+    login,
+    logout,
 }
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,9 +3,10 @@ const usersControler = require('../controllers/usersController.js')
 
 usersRouter.get('/', usersControler.getAll);
 usersRouter.get('/:id', usersControler.getOne);
-usersRouter.put('/:id', usersControler.updateOne);
-usersRouter.post('/register', usersControler.register);
-usersRouter.delete('/:id', usersControler.deleteOne);
-
+usersRouter.put('/:id/update', usersControler.updateOne);
+usersRouter.post('/register', usersControler.createOne);
+usersRouter.post('/login', usersControler.login);
+usersRouter.post('/logout', usersControler.logout);
+usersRouter.delete('/:id/delete', usersControler.deleteOne);
 
 module.exports = usersRouter;
