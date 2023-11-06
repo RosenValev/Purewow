9bd63aac7dc7be3531af1529ce585ff7099a3775
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-11-06 18:55:46 +0200

    forms login and register corrected

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -3,20 +3,22 @@ export default function Login() {
         <div className="login-register-div">
             <div className="login-register-box">
                 <h2>Welcome</h2>
-                <form method="POST">
-                    <p>
-                        <label>Username:</label>
-                        <input type="text" name="username" placeholder="Enter your username" />
-                    </p>
-                    <p>
-                        <label>Password:</label>
-                        <input type="password" name="password" placeholder="Enter your password" />
-                    </p>
-                    <button className="login-reg-button" type="submit">Login</button>
+                <form >
+                    <div>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name="username" id="username" placeholder="Enter your username" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password" id="password" placeholder="Enter your password" />
+                    </div>
+                    <div>
+                        <button className="login-reg-button" type="submit">Login</button>
+                    </div>
                 </form>
-                <p>
-                    Don't have account? <a href="/users/register">Register</a>
-                </p>
+                <div>
+                    <p>Don't have account? <a href="#">Register</a></p>
+                </div>
             </div>
 
         </div>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -3,24 +3,30 @@ export default function Register() {
         <div className="login-register-div">
             <div className="login-register-box">
                 <h2>Welcome</h2>
-                <form method="POST">
-                    <label>Username:</label>
-                    <input type="text" name="username" placeholder="Username" />
-
-                    <label>Email:</label>
-                    <input type="email" name="email" placeholder="Email" />
-
-                    <label>Password:</label>
-                    <input type="password" name="password" placeholder="Password" />
-
-                    <label>Repeat password:</label>
-                    <input type="password" name="repeatPassword" placeholder="Repeat Password" />
-
-                    <button className="login-reg-button" type="submit">Register</button>
+                <form >
+                    <div>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name="username" id="username" placeholder="Enter your username" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name="email" id="email" placeholder="Enter your email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password" id="password" placeholder="Enter your password" />
+                    </div>
+                    <div>
+                        <label htmlFor="repeatPassword">Repeat password:</label>
+                        <input type="password" name="repeatPassword" id="repeatPassword" placeholder="Repeat password" />
+                    </div>
+                    <div>
+                        <button className="login-reg-button" type="submit">Register</button>
+                    </div>
                 </form>
-                <p>
-                    Have an account? <a href="/users/login">Login</a>
-                </p>
+                <div>
+                    <p>  Have an account? <a href="#">Login</a></p>
+                </div>
             </div>
         </div>
     );
