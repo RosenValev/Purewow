5ef526ee0cc3ff513c7fd11cf7f14dbf65aea1b6
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-11-09 18:29:48 +0200

    added getAll recipies and getById

--- server/src/controllers/recipiesController.js
+++ server/src/controllers/recipiesController.js
@@ -15,7 +15,42 @@ const createRecipie = async (req, res) => {
         console.log(err.message);
         res.status(500).json({ message: err.message });
     }
+};
+
+//GET ALL 
+const getAllRecipies = async (req, res) => {
+    try {
+        const recipies = await Recipie.find().populate('owner');
+        if (!recipies) {
+            return res.status(404).json({ message: `Recipies not found` });
+        }
+        res.status(200).json(recipies);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: err.message });
+    }
 }
+
+//GET BY ID
+const getRecipieById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (!id) {
+            return res.status(404).json({ message: `Id is required` });
+        }
+        const recipie = await Recipie.findById(id).populate('owner');
+        if (!recipie) {
+            return res.status(404).json({ message: `Recipie not found` });
+        }
+        res.status(200).json(recipie);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: err.message });
+    }
+}
+
 module.exports = {
     createRecipie,
+    getAllRecipies,
+    getRecipieById
 }
