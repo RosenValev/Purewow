6d0c13a62e2acf72283d77e5dfb0669dad7f32df
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-12-01 10:01:36 +0200

    Add AuthGuard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,28 +12,27 @@ import Login from "./components/Auth/Login.jsx"
 import Register from "./components/Auth/Register.jsx"
 import Logout from "./components/Logout/Logout.jsx"
 import Footer from "./components/Footer/Footer.jsx"
-
+import AuthGuard from "./components/AuthGuard/AuthGuard.jsx"
 
 function App() {
     return (
         <AuthProvider>
-            <>
-                <Navigation />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/recipies" element={<RecipieList />} />
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/recipies" element={<RecipieList />} />
+                <Route path="/recipies/:id" element={<DetailsRecipie />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route element={<AuthGuard />}>
                     <Route path="/create-recipie" element={<CreateRecipie />} />
                     <Route path="/recipies/edit/:id" element={<EditRecipie />} />
-                    <Route path="/recipies/:id" element={<DetailsRecipie />} />
                     <Route path="/my-profile" element={<MyProfile />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
                     <Route path="/logout" element={<Logout />} />
-                </Routes>
-                <Footer />
-            </>
+                </Route>
+            </Routes>
+            <Footer />
         </AuthProvider>
     )
 }
-
-export default App
+export default App;
